4655950c9e2ce68d897b9cbb323e056378d2e9ab
Isaac Mushimiyimana <mushimiyimanais123@gmail.com>
2024-04-27 12:36:46 +0200

    other commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { Route, useParams } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import './App.css'
 import  { CartProvider } from './CartContext/context'
-import Routing from './routes/Routes'
 import { Datastore } from './assets/data';
-
+import Layout from './Layout';
+import Home from './pages/home';
+import CartPage from './pages/cartlist';
+import About from './pages/About';
+import Refund from './pages/refund';
+import Notfound from './pages/notfound';
+import Payment from './pages/checkout-to-pay'
 function App() {
   console.log(Datastore);
   return (
     <>
     <CartProvider>
-      <Routing />
+     <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/cart" element={<CartPage/>} />
+            <Route path="/refund" element={<Refund />} />
+            <Route path="/checkout" element={<Payment />} />
+            <Route path="*" element={<Notfound />} />
+          </Route>
+     </Routes>
     </CartProvider>
     </>
   )
