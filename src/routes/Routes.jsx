845713d1195a8914b76d7e43a014f602c38efe26
Isaac Mushimiyimana <mushimiyimanais123@gmail.com>
2024-04-27 11:58:59 +0200

    error remo

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,13 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from '../Layout';
 import Home from '../pages/home';
 import NotFound from '../pages/notfound';
+import About from '../pages/About';
 
 export default function Routing() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        {/* Define other nested routes within Layout if needed */}
+        <Route path="/about" element={<About />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
