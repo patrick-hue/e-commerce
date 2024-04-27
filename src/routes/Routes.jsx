3720671caf46ef7d318ad15b7ad49f7c4ca1f5e7
Isaac Mushimiyimana <mushimiyimanais123@gmail.com>
2024-04-27 11:37:41 +0200

    error

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Layout from '../Layout'
-import Home from '../pages/home'
-import Notfound from '../pages/notfound'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from '../Layout';
+import Home from '../pages/home';
+import NotFound from '../pages/notfound';
 
 export default function Routing() {
   return (
     <Routes>
-        <Route path='/' element={<Layout />}>
-            <Route path='/' element={<Home />} />
-        </Route>
-        <Route path='*' element={<Notfound />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        {/* Define other nested routes within Layout if needed */}
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
-  )
+  );
 }
