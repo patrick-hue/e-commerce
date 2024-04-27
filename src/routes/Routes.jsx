53cdebcbe5680fe1f367b07f30650ad601e33963
Isaac Mushimiyimana <mushimiyimanais123@gmail.com>
2024-04-27 11:46:43 +0200

    error remo

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,7 +8,7 @@ export default function Routing() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path='/*' element={<Home />} />
+        <Route path='' element={<Home />} />
         {/* Define other nested routes within Layout if needed */}
       </Route>
       <Route path="*" element={<NotFound />} />
